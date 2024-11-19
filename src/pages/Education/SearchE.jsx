aa52c6c890b401a9b6bfcfb4ea2c13b727d68eb8
Dongmyoung Min <zmar@Dongmyoungui-MacBookAir.local>
2024-11-19 13:31:06 +0900

    🎉Start: Search 페이지 초기 설정

---
+++ src/pages/Education/SearchE.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as C from "../../styles/CommonStyle";
+
+function SearchE() {
+  return (
+    <C.Page>
+      <C.Center>
+        <C.PageSpace></C.PageSpace>
+      </C.Center>
+    </C.Page>
+  );
+}
+
+export default SearchE;
---
+++ src/pages/Home/Search.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as C from "../../styles/CommonStyle";
+
+function Search() {
+  return (
+    <C.Page>
+      <C.Center>
+        <C.PageSpace></C.PageSpace>
+      </C.Center>
+    </C.Page>
+  );
+}
+
+export default Search;
